782a0c2fed80d512f9038a054c7b6fd7568049ee
joshkat <joshua02k@gmail.com>
2022-07-26 20:14:35 -0400

    added functional drawing pad

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const drawingContainer = document.getElementById('drawingContainer');
+let px = 16;
 
-for(i = 0; i < 256; i++){
+for(i = 0; i < (px*px); i++){
     var newPixel = document.createElement('div');
     newPixel.classList.add('pixels');
     drawingContainer.appendChild(newPixel);
 }
 
+drawingContainer.addEventListener('mouseover', (e) => {
+    e.target.style.background = 'blue';
+    e.stopPropagation();
+});
